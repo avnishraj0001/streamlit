c793bc31c49480c332088f7fa164c2e30c81a526
avnishraj0001 <avnishrajhero1202@gmail.com>
2026-02-07 20:20:47 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -4,7 +4,7 @@ import google.generativeai as genai
 st.title("Welcome to gemini models viewer")
 
 user_input = st.text_input("Enter your google api key: ")
-genai.configure(api_key = "AIzaSyBuZxJBXZj2jqdw3Hx6tKUipgq54Yfb2uc")
+genai.configure(api_key = "AIzaSyA82Gr5dZoGN42oRcvIve7LlNhJX-tEaHA")
 
 genai.GenerativeModel("models/gemini-2.5-flash")
 if user_input:
@@ -13,3 +13,4 @@ if user_input:
     st.write("Generated content")
 
     st.write(response.text)
+
